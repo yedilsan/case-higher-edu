285b4b6dae1a30c93eb34fec4a3435bdca6a6da2
yedilsan <yedilsanatovv@gmail.com>
2023-12-26 16:32:20 +0600

    added filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import "./App.css";
 import CollapseMenu from "./components/CollapseMenu";
+import Filter from "./components/Filter";
 
 function App() {
   return (
-    <>
-      <CollapseMenu />
-    </>
+    <div
+      style={{
+        display: "flex",
+        position: "relative",
+        flexDirection: "column",
+      }}
+    >
+      <div
+        style={{
+          marginLeft: "auto",
+        }}
+      >
+        <Filter />
+      </div>
+      <div>
+        <CollapseMenu />
+      </div>
+    </div>
   );
 }
 
