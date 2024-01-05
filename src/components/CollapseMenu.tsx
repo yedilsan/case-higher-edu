70a90f48a2e735c59c5d241f3310df877dc9662c
yedilsan <yedilsanatovv@gmail.com>
2024-01-05 11:32:30 +0600

    Collapse Menu: Исправлене импорты графиков, добавленые новые с использованием пропсов, удалены некоторые стили

--- src/components/CollapseMenu.tsx
+++ src/components/CollapseMenu.tsx
@@ -1,125 +1,129 @@
-import { CollapseProps, Collapse, Col, Row } from "antd";
-import TreeData from "../components/TreeTable";
-import PpsCitizenship from "../components/Graphs/PPS/PpsCitizenship";
-import ScienceRank from "../components/Graphs/PPS/ScienceRank";
-import AgeChart from "../components/Graphs/PPS/AgeChart";
-import StudLineChart from "../components/Graphs/Students/StudLineChart";
-import StudStackedColChart from "../components/Graphs/Students/StudStackedColChart";
-import StudPieChart from "../components/Graphs/Students/StudPieChart";
-import StudColumnChart from "../components/Graphs/Students/StudColumnChart";
-import ProjectsChart from "../components/Graphs/ProjectActivities/ProjectsChart";
-import StudentEmployment from "../components/Graphs/StudentEmployment/StudentEmployment";
-import "./component.css";
+import { Col, Collapse, CollapseProps, Row } from 'antd';
+import TreeData from '../components/TreeTable';
+import ColumnChart from './Graphs/ColumnChart';
+import ColumnDateAxisChart from './Graphs/ColumnDateAxisChart';
+import HorizontalBarChart from './Graphs/HorizontalBarChart';
+import PieChart from './Graphs/PieChart';
+import ProjectsChart from './Graphs/ProjectsChart';
+import StackedColumnChart from './Graphs/StackedColumnChart';
+import StudentEmployment from './Graphs/StudentEmploymentChart';
+import './component.css';
 
-const items: CollapseProps["items"] = [
-  {
-    key: "1",
-    label: "Образовательные программы",
-    children: (
-      <div className="tree-wrapper">
-        <div>
-          <TreeData level="Бакалавриат" />
-        </div>
-        <div>
-          <TreeData level="Магистратура" />
-        </div>
-        <div>
-          <TreeData level="Докторантура" />
-        </div>
-      </div>
-    ),
-  },
-  {
-    key: "2",
-    label: "ППС",
-    children: (
-      <Row className="charts">
-        <Col className="chart_layout">
-          <PpsCitizenship />
-        </Col>
-        <Col className="chart_layout">
-          <ScienceRank dimension="science_rank" />
-        </Col>
-        <Col className="chart_layout">
-          <ScienceRank dimension="science_degree" />
-        </Col>
-        <Col className="chart_layout">
-          <AgeChart dimension="age" />
-        </Col>
-      </Row>
-    ),
-  },
-  {
-    key: "3",
-    label: "Студенты",
-    children: (
-      <Row className="charts">
-        <Col className="chart_layout">
-          <StudLineChart dimension="finance_type" />
-        </Col>
-        <Col className="chart_layout">
-          <StudLineChart dimension="specialty" />
-        </Col>
-        <Col className="chart_layout">
-          <StudStackedColChart />
-        </Col>
-        <Col className="chart_layout">
-          <StudColumnChart dimension="citizenship" />
-        </Col>
-        <Col className="chart_layout">
-          <StudPieChart />
-        </Col>
-        <Col className="chart_layout">
-          <StudColumnChart dimension="socially_vulnerable" />
-        </Col>
-      </Row>
-    ),
-  },
-  {
-    key: "4",
-    label: "Проектная деятельность",
-    children: (
-      <div
-        style={{ margin: "10px", display: "flex", justifyContent: "center" }}
-      >
-        <div>
-          <h3>Объем финансирования проектов по источнику</h3>
-          <ProjectsChart />
-        </div>
-      </div>
-    ),
-  },
-  {
-    key: "5",
-    label: "Трудоустройство выпускников",
-    children: (
-      <div
-        style={{ margin: "10px", display: "flex", justifyContent: "center" }}
-      >
-        <div>
-          <h3>Показатели трудоустройства выпускников в процентах</h3>
-          <StudentEmployment />
-        </div>
-      </div>
-    ),
-  },
-  {
-    key: "6",
-    label: "Публикации",
-    children: <p>Нет данных</p>,
-  },
+const items: CollapseProps['items'] = [
+	{
+		key: '1',
+		label: 'Образовательные программы',
+		children: (
+			<div className='tree-wrapper'>
+				<div>
+					<TreeData level='Бакалавриат' />
+				</div>
+				<div>
+					<TreeData level='Магистратура' />
+				</div>
+				<div>
+					<TreeData level='Докторантура' />
+				</div>
+			</div>
+		),
+	},
+	{
+		key: '2',
+		label: 'ППС',
+		children: (
+			<Row className='charts'>
+				<Col className='chart_layout'>
+					<PieChart measures='pps.count' dimension='pps.citizenship' />
+				</Col>
+				<Col className='chart_layout'>
+					<HorizontalBarChart
+						measures='pps.count'
+						dimension='pps.science_rank'
+					/>
+				</Col>
+				<Col className='chart_layout'>
+					<HorizontalBarChart
+						measures='pps.count'
+						dimension='pps.science_degree'
+					/>
+				</Col>
+				<Col className='chart_layout'>
+					<ColumnDateAxisChart measures='pps.count' dimension='pps.age' />
+				</Col>
+			</Row>
+		),
+	},
+	{
+		key: '3',
+		label: 'Студенты',
+		children: (
+			<Row className='charts'>
+				<Col className='chart_layout'>
+					<HorizontalBarChart
+						measures='student.count'
+						dimension='student.finance_type'
+					/>
+				</Col>
+				<Col className='chart_layout'>
+					<HorizontalBarChart
+						measures='student.count'
+						dimension='student.specialty'
+					/>
+				</Col>
+				<Col className='chart_layout'>
+					<StackedColumnChart
+						measures='student.count'
+						dimension={['student.admission_year', 'student.study_language']}
+					/>
+				</Col>
+				<Col className='chart_layout'>
+					<ColumnChart
+						measures='student.count'
+						dimension='student.citizenship'
+					/>
+				</Col>
+				<Col className='chart_layout'>
+					<PieChart
+						measures='student.count'
+						dimension='student.last_education_type'
+					/>
+				</Col>
+				<Col className='chart_layout'>
+					<ColumnChart
+						measures='student.count'
+						dimension='student.socially_vulnerable'
+					/>
+				</Col>
+			</Row>
+		),
+	},
+	{
+		key: '4',
+		label: 'Проектная деятельность',
+		children: <ProjectsChart />,
+	},
+	{
+		key: '5',
+		label: 'Трудоустройство выпускников',
+		children: <StudentEmployment />,
+	},
+	{
+		key: '6',
+		label: 'Публикации',
+		children: <p>Нет данных</p>,
+	},
 ];
 
 const CollapseMenu = () => {
-  return (
-    <>
-      <div className="page__wrapper">
-        <div className="page__collapse">
-          <Collapse accordion items={items} size="large" />
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className='page__wrapper'>
+				<div className='page__collapse'>
+					<Collapse accordion items={items} size='large' />
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default CollapseMenu;
