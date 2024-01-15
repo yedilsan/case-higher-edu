b8a495940e1434d58dfe80e2588df54c42d60a65
yedilsan <yedilsanatovv@gmail.com>
2024-01-15 16:48:45 +0600

    useCubeData перемещен в папку hooks

---
+++ src/hooks/useCubeData.ts
@@ -0,0 +1,28 @@
+import { TQueryOrderObject } from '@cubejs-client/core';
+import { useCubeQuery } from '@cubejs-client/react';
+import cubejsApi from '../services/cubejsConfig';
+
+interface UseCubeDataProps {
+	measures?: string;
+	dimension?: string[];
+	orderBy?: string[];
+}
+
+export const useCubeData = ({
+	measures,
+	dimension,
+	orderBy,
+}: UseCubeDataProps) => {
+	const { resultSet, isLoading, error } = useCubeQuery(
+		{
+			measures: measures ? [measures] : undefined,
+			dimensions: dimension,
+			order: orderBy
+				? ({ [`${orderBy[0]}`]: orderBy[1] } as TQueryOrderObject)
+				: undefined,
+		},
+		{ cubejsApi }
+	);
+
+	return { resultSet, isLoading, error };
+};
